2561bfae0fbe47823b1cdb1cc99aca1aea2e11c2
vamseeuxd <vamsee.uxd@gmail.com>
2024-08-27 21:16:59 +0530

    added pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(
+        (mod) => mod.DashboardComponent
+      ),
+  },
+];
